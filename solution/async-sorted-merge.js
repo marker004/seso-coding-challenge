92b04cc5141f6ebaf8cc2e6fba2e5455ebf69230
mreckard <mreckard@shutterstock.com>
2023-10-20 16:23:01 -0400

    load everything into a big array async

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -4,6 +4,40 @@
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    const allLogs = [];
+
+    const nextLog = (logSource) => {
+      return logSource.popAsync().then((result) => {
+        if (result) {
+          allLogs.push(result);
+
+          return nextLog(logSource);
+        } else {
+          return result;
+        }
+      });
+    };
+
+    Promise.all(
+      logSources.map((logSource) => {
+        return nextLog(logSource);
+      })
+    ).then(() => {
+      allLogs.sort((a, b) => {
+        return a.date - b.date;
+      });
+
+      allLogs.sort((a, b) => {
+        return a.date - b.date;
+      });
+
+      console.log(allLogs.length);
+      allLogs.forEach((log) => {
+        printer.print(log);
+      });
+
+      printer.done();
+      resolve(console.log("Async sort complete."));
+    });
   });
 };
