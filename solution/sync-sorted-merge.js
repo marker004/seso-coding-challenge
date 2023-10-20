e435dafb8b7d530d24848bb33477d70377ee0e62
mreckard <mreckard@shutterstock.com>
2023-10-20 12:10:37 -0400

    load everything into a big array

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,23 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const allLogs = [];
+
+  logSources.forEach((logSource) => {
+    while (!logSource.drained) {
+      const log = logSource.pop();
+      if (log) allLogs.push(log);
+    }
+  });
+
+  allLogs.sort((a, b) => {
+    return a.date - b.date;
+  });
+
+  allLogs.forEach((log) => {
+    printer.print(log);
+  });
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
