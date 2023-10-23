570415fd27d566a7f2d0bc3f520935764293a10b
mreckard <mreckard@shutterstock.com>
2023-10-23 13:00:22 -0400

    apply solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,27 @@
 "use strict";
 
+const { everybodyBubbleUp, settleDownYall, remove } = require("../lib/utils");
+const { logSourceSortingCriterion } = require("../lib/log-source-utils");
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
+    everybodyBubbleUp(logSources, logSourceSortingCriterion);
+
+    while (logSources.length) {
+      if (!logSources[0].drained) {
+        printer.print(logSources[0].last);
+        logSources[0].pop();
+
+        settleDownYall(logSources, logSourceSortingCriterion);
+      } else {
+        remove(logSources[0], logSources, logSourceSortingCriterion);
+      }
+    }
+
+    printer.done();
+
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,24 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const { everybodyBubbleUp, settleDownYall, remove } = require("../lib/utils");
+const { logSourceSortingCriterion } = require("../lib/log-source-utils");
+
 module.exports = (logSources, printer) => {
+  everybodyBubbleUp(logSources, logSourceSortingCriterion);
+
+  while (logSources.length) {
+    if (!logSources[0].drained) {
+      printer.print(logSources[0].last);
+      logSources[0].pop();
+
+      settleDownYall(logSources, logSourceSortingCriterion);
+    } else {
+      remove(logSources[0], logSources, logSourceSortingCriterion);
+    }
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
